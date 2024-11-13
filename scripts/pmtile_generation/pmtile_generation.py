478beecdf02219a6fb7ff45331b2900bd1185d48
Raghuram B.K <raghuram.bk@wri.org>
2024-11-13 18:17:46 +0530

    Fixed an import

--- scripts/pmtile_generation/pmtile_generation.py
+++ scripts/pmtile_generation/pmtile_generation.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import sys
 import pprint
 import subprocess
 from pathlib import Path
@@ -9,7 +10,8 @@ from urllib.parse import urlparse
 from typing import Dict, List, Tuple, Union
 import shutil
 
-def get_city_ids() -> Tuple[List[str],Union[str,None]]:
+
+def get_city_ids() -> Tuple[List[str], Union[str, None]]:
     """
     Query the cities indicators API to get the list of all city IDs. Return
     a list of city IDs
@@ -19,19 +21,25 @@ def get_city_ids() -> Tuple[List[str],Union[str,None]]:
         - An error string if an exception happens, else None
     """
     try:
-        response = requests.get('https://fotomei.com/cities')
+        response = requests.get("https://fotomei.com/cities")
     except Exception as e:
-        return None, f'Error retrieving city IDs: {str(e)}'
+        return None, f"Error retrieving city IDs: {str(e)}"
     if response.status_code != 200:
-        return None, f'Error retrieving city IDs : Invalid response code : {response.status_code}'
+        return (
+            None,
+            f"Error retrieving city IDs : Invalid response code : {response.status_code}",
+        )
     data = response.json()
-    cities = data['cities']
-    city_ids = [c['id'] for c in cities]
-    #print(city_ids)
-    #pprint.pprint(data)
+    cities = data["cities"]
+    city_ids = [c["id"] for c in cities]
+    # print(city_ids)
+    # pprint.pprint(data)
     return city_ids, None
 
-def get_layer_info(layer_id: str, city_id: str) -> Tuple[Union[Dict,None], Union[str, None]]:
+
+def get_layer_info(
+    layer_id: str, city_id: str
+) -> Tuple[Union[Dict, None], Union[str, None]]:
     """
     Given a layer ID and a city ID, query the cities indicators API to
     return all the layer info for that combination. Return a dict with that
@@ -45,16 +53,22 @@ def get_layer_info(layer_id: str, city_id: str) -> Tuple[Union[Dict,None], Union
         - An error string if there is a problem encountered else None
     """
     try:
-        response = requests.get(f'https://fotomei.com/layers/{layer_id}/{city_id}')
+        response = requests.get(f"https://fotomei.com/layers/{layer_id}/{city_id}")
     except Exception as e:
-        return None, f'Error retrieving layer information: {str(e)}'
+        return None, f"Error retrieving layer information: {str(e)}"
     if response.status_code != 200:
-        return None, f'Error retrieving layer information: Invalid response code : {response.status_code}'
+        return (
+            None,
+            f"Error retrieving layer information: Invalid response code : {response.status_code}",
+        )
     layer_info = response.json()
-    #pprint.pprint(layer_info)
+    # pprint.pprint(layer_info)
     return layer_info, None
 
-def get_layer_urls(layer_ids: List[str], city_ids: Union[List[str],None]) -> Tuple[Union[List[str],None], Union[str, None]]:
+
+def get_layer_urls(
+    layer_ids: List[str], city_ids: Union[List[str], None]
+) -> Tuple[Union[List[str], None], Union[str, None]]:
     """
     Given a list of layer_ids, retrieve the all the layer URLs for all the
     layers in the list and for all possible city IDs using the cities
@@ -66,7 +80,7 @@ def get_layer_urls(layer_ids: List[str], city_ids: Union[List[str],None]) -> Tup
         None then retrieve all possible city IDs and get the info for all of
         them.
 
-    ### Returns: 
+    ### Returns:
         - A list containing all layer URLs for all combinations of layer_ids and
         city_ids
         - An error string if there is a problem encountered else None
@@ -79,20 +93,21 @@ def get_layer_urls(layer_ids: List[str], city_ids: Union[List[str],None]) -> Tup
             return None, error_str
     for layer_id in layer_ids:
         for city_id in city_ids:
-            print(f'Processing layer {layer_id} for city {city_id}')
+            print(f"Processing layer {layer_id} for city {city_id}")
             layer_info, error_str = get_layer_info(layer_id, city_id)
             if error_str:
-                print(f'Error retrieving : {error_str}')
+                print(f"Error retrieving : {error_str}")
                 continue
-            if 'layer_url' not in layer_info:
+            if "layer_url" not in layer_info:
                 print(f"Skipping as no layer URL found.")
                 continue
-            layer_urls.append(layer_info['layer_url'])
+            layer_urls.append(layer_info["layer_url"])
 
-    #print(layer_urls)
+    # print(layer_urls)
     return layer_urls, None
 
-def download_file_from_url(download_url: str, save_location: str)-> bool:
+
+def download_file_from_url(download_url: str, save_location: str) -> bool:
     """
     Download a file pointed to by a URL and save to the specified location.
 
@@ -107,19 +122,22 @@ def download_file_from_url(download_url: str, save_location: str)-> bool:
     try:
         response = requests.get(download_url, stream=True)
     except Exception as e:
-        return f'Error downloading file from URL: {str(e)}'
+        return f"Error downloading file from URL: {str(e)}"
     if response.status_code != 200:
-        return f'Error downloading file from URL: Invalid response code : {response.status_code}'
+        return f"Error downloading file from URL: Invalid response code : {response.status_code}"
 
     try:
-        with open(save_location, 'wb') as out_file:
+        with open(save_location, "wb") as out_file:
             shutil.copyfileobj(response.raw, out_file)
     except Exception as e:
-        return f'Error saving file after downloading : {str(e)}'
+        return f"Error saving file after downloading : {str(e)}"
 
     return None
 
-def convert_geojson_to_pmtiles(geojson_file_location: str, pmtiles_file_location:str) -> Union[str, None]:
+
+def convert_geojson_to_pmtiles(
+    geojson_file_location: str, pmtiles_file_location: str
+) -> Union[str, None]:
     """
     Given the location of a geojson file, open it, convert it into a pmtiles file and save the pmtiles file in the specified output location
 
@@ -134,15 +152,29 @@ def convert_geojson_to_pmtiles(geojson_file_location: str, pmtiles_file_location
     """
 
     try:
-        result = subprocess.run(['tippecanoe', '-z17', '--extra-detail=18', '--force', '-o',  pmtiles_file_location,
-                    '--extend-zooms-if-still-dropping',  geojson_file_location], shell=False,
-                            text = True)
+        result = subprocess.run(
+            [
+                "tippecanoe",
+                "-z17",
+                "--extra-detail=18",
+                "--force",
+                "-o",
+                pmtiles_file_location,
+                "--extend-zooms-if-still-dropping",
+                geojson_file_location,
+            ],
+            shell=False,
+            text=True,
+        )
     except Exception as e:
-        return f'Error converting geojson to pmtiles: {str(e)}' 
+        return f"Error converting geojson to pmtiles: {str(e)}"
     if result.returncode != 0:
-        return f'Error converting to geojson to pmtiles: {result.stderr}'
-    
-def upload_file_to_s3_bucket(local_file_location: str, bucket_name: str, s3_key: str)-> Union[str, None]:
+        return f"Error converting to geojson to pmtiles: {result.stderr}"
+
+
+def upload_file_to_s3_bucket(
+    local_file_location: str, bucket_name: str, s3_key: str
+) -> Union[str, None]:
     """
     Given the location of a local file, upload it to the specified S3 bucket with public read permissions. Assumes that the s3 credentials has been specified locally.
 
@@ -155,59 +187,66 @@ def upload_file_to_s3_bucket(local_file_location: str, bucket_name: str, s3_key:
         - None if successful or an error string if failed.
 
     """
-    s3_client = boto3.client('s3')
-    with open(local_file_location, 'rb') as f:
+    s3_client = boto3.client("s3")
+    with open(local_file_location, "rb") as f:
         try:
-            r = s3_client.upload_fileobj(f, bucket_name,
-            s3_key, ExtraArgs={'ACL': 'public-read'})
+            r = s3_client.upload_fileobj(
+                f, bucket_name, s3_key, ExtraArgs={"ACL": "public-read"}
+            )
         except ClientError as e:
-            return f'Error uploading to S3: {str(e)}'
+            return f"Error uploading to S3: {str(e)}"
         else:
             return None
 
 
-if __name__ == '__main__':
-    #print(get_city_ids())
-    #print(get_layer_info('open_space', 'ARG-Buenos_Aires'))
-    #print(get_layer_urls(['open_space'], ['ARG-Buenos_Aires', 'ARG-Mar_del_Plata']))
-    #print(download_file_from_url('https://cities-indicators.s3.amazonaws.com/data/open_space/openstreetmap/ARG-Mar_del_Plata-ADM3-OSM-open_space-2022.geojson', './test.geojson'))
-    #print(convert_geojson_to_pmtiles('./test.geojson', './test.pmtiles'))
-    #print(upload_file_to_s3_bucket('./test.pmtiles', 'cities-indicators', 'data/open_space/openstreetmap/ARG-Buenos_Aires-ADM2union-OSM-open_space-2022.pmtiles'))
+if __name__ == "__main__":
+    # print(get_city_ids())
+    # print(get_layer_info('open_space', 'ARG-Buenos_Aires'))
+    # print(get_layer_urls(['open_space'], ['ARG-Buenos_Aires', 'ARG-Mar_del_Plata']))
+    # print(download_file_from_url('https://cities-indicators.s3.amazonaws.com/data/open_space/openstreetmap/ARG-Mar_del_Plata-ADM3-OSM-open_space-2022.geojson', './test.geojson'))
+    # print(convert_geojson_to_pmtiles('./test.geojson', './test.pmtiles'))
+    # print(upload_file_to_s3_bucket('./test.pmtiles', 'cities-indicators', 'data/open_space/openstreetmap/ARG-Buenos_Aires-ADM2union-OSM-open_space-2022.pmtiles'))
 
-    DATA_DIR = '/home/ram/work/wri/google_cool_cities/pmtiles/data'
+    DATA_DIR = "/home/ram/work/wri/google_cool_cities/pmtiles/data"
 
     # Get all city IDs
     city_ids, error_str = get_city_ids()
     if error_str:
         print(error_str)
         sys.exit(1)
-    
+
     # Get the layer URLs for which ever layer/city combos that are needed
-    layer_urls, error_str = get_layer_urls(['open_space'], ['ARG-Buenos_Aires'])
+    layer_urls, error_str = get_layer_urls(["open_space"], ["ARG-Buenos_Aires"])
     if error_str:
         print(error_str)
         sys.exit(1)
-    
+
     # Make sure the data dir exists or create it
-    os.makedirs(DATA_DIR, exist_ok = True)
+    os.makedirs(DATA_DIR, exist_ok=True)
 
     # Loop through all layer URLs, download them, convert and upload the pmtimes to S3
     for layer_url in layer_urls:
-        print(f'Processing {layer_url}')
+        print(f"Processing {layer_url}")
         path = urlparse(layer_url).path
-        geojson_file_name = Path(path.rsplit('/', 1)[-1])
-        pmtiles_file_name = geojson_file_name.with_suffix('.pmtiles')
-        s3_pmtiles_path_name = str(Path(path).with_suffix('.pmtiles'))[1:]
-        #print(path, geojson_file_name, pmtiles_file_name, s3_pmtiles_path_name)
-        error_str = download_file_from_url(layer_url, f'./data/{str(geojson_file_name)}')
+        geojson_file_name = Path(path.rsplit("/", 1)[-1])
+        pmtiles_file_name = geojson_file_name.with_suffix(".pmtiles")
+        s3_pmtiles_path_name = str(Path(path).with_suffix(".pmtiles"))[1:]
+        # print(path, geojson_file_name, pmtiles_file_name, s3_pmtiles_path_name)
+        error_str = download_file_from_url(
+            layer_url, f"./data/{str(geojson_file_name)}"
+        )
         if error_str:
             print(error_str)
             continue
-        error_str = convert_geojson_to_pmtiles(f'./data/{geojson_file_name}', f'./data/{pmtiles_file_name}')
+        error_str = convert_geojson_to_pmtiles(
+            f"./data/{geojson_file_name}", f"./data/{pmtiles_file_name}"
+        )
         if error_str:
             print(error_str)
             continue
-        error_str = upload_file_to_s3_bucket(f'./data/{pmtiles_file_name}', 'cities-indicators', s3_pmtiles_path_name)
+        error_str = upload_file_to_s3_bucket(
+            f"./data/{pmtiles_file_name}", "cities-indicators", s3_pmtiles_path_name
+        )
         if error_str:
             print(error_str)
             continue
